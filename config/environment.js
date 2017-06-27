3b9167ec297bf777bca2d94aec660f183659422a
null null null <null@affinitygamescollective.org>
2017-06-27 14:29:28 -0700

    change name to client

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'timeline',
+    modulePrefix: 'client',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
