14014dbdec040af7b773f8e6b441516927072f99
null null null <null@affinitygamescollective.org>
2017-09-22 11:29:01 -0400

    top-bar with dummy content

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['top-bar'],
+
   modalManager: Ember.inject.service('modal-manager'),
   session: Ember.inject.service('session'),
 
