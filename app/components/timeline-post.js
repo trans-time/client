9d56fad0d0dda3fe475ce4d55e5d2d4e7cf22fef
null null null <null@affinitygamescollective.org>
2017-09-25 09:42:01 -0400

    ensure post is scrolled to top

--- app/components/timeline-post.js
+++ app/components/timeline-post.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['timeline-post']
+  classNames: ['timeline-post'],
+
+  didReceiveAttrs(...args) {
+    this._super(...args);
+
+    if (this.$()) this.$().animate({ scrollTop: 0 }, 'fast');
+  }
 });
