d23e830ff2bd26ec4eb247fb88f7e5ce7376699a
null null null <null@affinitygamescollective.org>
2017-11-16 12:55:26 -0500

    handle image upload concurrency again

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 import { isBlank, typeOf } from '@ember/utils';
 import Route from '@ember/routing/route';
 import { task } from 'ember-concurrency';
+import { Promise, all } from 'rsvp';
 import config from '../../config/environment';
 import RouteTitleMixin from 'client/mixins/route-title';
 
@@ -37,6 +38,14 @@ export default Route.extend(RouteTitleMixin, {
     }
   }),
 
+  uploadAndSave: task(function * (model) {
+    const promises = [];
+    model.get('panels').forEach((panel) => {
+      promises.push(get(this, 'uploadImageTask').perform(panel));
+    });
+    yield all(promises);
+    model.save();
+  }),
 
   uploadImageTask: task(function * (panel) {
     const file = panel.get('file');
@@ -51,45 +60,40 @@ export default Route.extend(RouteTitleMixin, {
       canvas.width = 1080;
       img.src = dataURL;
 
-      img.onload = () => {
-        if (img.naturalHeight / img.naturalWidth < 1350 / 1080) {
-          const width = (img.naturalHeight / 1350) * 1080;
-          const percentX = panel.get('positioning.x') / 100;
-          const startX = (percentX * img.naturalWidth) - (percentX * width);
+      yield new Promise((resolve) => {
+        img.onload = () => {
+          if (img.naturalHeight / img.naturalWidth < 1350 / 1080) {
+            const width = (img.naturalHeight / 1350) * 1080;
+            const percentX = panel.get('positioning.x') / 100;
+            const startX = (percentX * img.naturalWidth) - (percentX * width);
 
-          canvas.getContext('2d').drawImage(img, startX, 0, width, img.naturalHeight, 0, 0, 1080, 1350);
-        } else {
-          const height = (img.naturalWidth / 1080) * 1350;
-          const percentY = panel.get('positioning.y') / 100;
-          const startY = (percentY * img.naturalHeight) - (percentY * height);
+            canvas.getContext('2d').drawImage(img, startX, 0, width, img.naturalHeight, 0, 0, 1080, 1350);
+          } else {
+            const height = (img.naturalWidth / 1080) * 1350;
+            const percentY = panel.get('positioning.y') / 100;
+            const startY = (percentY * img.naturalHeight) - (percentY * height);
 
-          canvas.getContext('2d').drawImage(img, 0, startY, img.naturalWidth, height, 0, 0, 1080, 1350);
+            canvas.getContext('2d').drawImage(img, 0, startY, img.naturalWidth, height, 0, 0, 1080, 1350);
+          }
+
+          canvas.toBlob((blob) => {
+            blob.name = get(file, 'name');
+            const [newFile] = this.get('queue')._addFiles([blob], 'blob');
+            newFile.upload(`${config.rootURL}images/upload`).then((result) => {
+              set(panel, 'src', get(result, 'body.data.attributes.src'));
+              panel.save().then(resolve);
+            });
+          }, 'image/jpeg');
         }
-
-        canvas.toBlob((blob) => {
-          blob.name = get(file, 'name');
-          const [newFile] = this.get('queue')._addFiles([blob], 'blob');
-          newFile.upload(`${config.rootURL}images/upload`).then((result) => {
-            set(panel, 'src', get(result, 'body.data.attributes.src'));
-            panel.save();
-          });
-        }, 'image/jpeg');
-      }
+      })
     } catch (e) {
       console.log(e);
     }
   }).maxConcurrency(3).enqueue(),
 
   actions: {
-    uploadFile(file, type) {
-      get(this, 'uploadImageTask').perform(file, type);
-    },
-
     submit(model) {
-      model.get('panels').forEach((panel) => {
-        get(this, 'uploadImageTask').perform(panel);
-      });
-      model.save();
+      this.get('uploadAndSave').perform(model);
     }
   }
 });
