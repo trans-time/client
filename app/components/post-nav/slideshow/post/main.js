2e0fa9c4e96bd64eb7d0f5bc8e32d7c103d6411b
null null null <null@affinitygamescollective.org>
2017-12-09 14:04:20 -0500

    screen nsfw content

--- app/components/post-nav/slideshow/post/main.js
+++ app/components/post-nav/slideshow/post/main.js
@@ -1,4 +1,6 @@
+import { computed } from '@ember/object';
 import { oneWay, or } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import SlideshowComponentMixin from 'client/mixins/slideshow-component';
 
@@ -9,5 +11,44 @@ export default Component.extend(SlideshowComponentMixin, {
   isOutgoing: oneWay('post.isOutgoing'),
   isIncoming: oneWay('post.isIncoming'),
   isBlank: oneWay('post.isBlank'),
-  shouldRenderPost: or('visible', 'post.shouldPrerender')
+  shouldRenderPost: or('visible', 'post.shouldPrerender'),
+
+  intl: service(),
+  messageBus: service(),
+  modalManager: service(),
+
+  didReceiveAttrs(...args) {
+    this._super(...args);
+
+    const nsfw = this.get('post.nsfw');
+
+    this.get('messageBus').subscribe('enabledNsfw', this, () => this.notifyPropertyChange('nsfw'));
+  },
+
+  nsfw: computed({
+    get() {
+      console.log(this.get('post.model.nsfw'), localStorage.getItem('showNsfwContent') || sessionStorage.getItem('showNsfwContent'))
+      return this.get('post.model.nsfw') && !(localStorage.getItem('showNsfwContent') || sessionStorage.getItem('showNsfwContent'));
+    }
+  }),
+
+  actions: {
+    viewNsfwForSession() {
+      new Promise((resolve, reject) => {
+        this.get('modalManager').open('confirmation-modal', resolve, reject, { content: this.get('intl').t('nsfw.confirmation') });
+      }).then(() => {
+        sessionStorage.setItem('showNsfwContent', true);
+        this.get('messageBus').publish('enabledNsfw');
+      });
+    },
+
+    viewNsfwForAlways() {
+      new Promise((resolve, reject) => {
+        this.get('modalManager').open('confirmation-modal', resolve, reject, { content: this.get('intl').t('nsfw.confirmation') });
+      }).then(() => {
+        localStorage.setItem('showNsfwContent', true);
+        this.get('messageBus').publish('enabledNsfw');
+      });
+    }
+  }
 });
