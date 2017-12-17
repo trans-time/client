4856c4612bffe39cd319f91c7d1f73f9747963a1
null null null <null@affinitygamescollective.org>
2017-12-16 21:22:35 -0500

    determine earliest and latest date for identities

--- app/components/identities-timeline.js
+++ app/components/identities-timeline.js
@@ -21,8 +21,32 @@ export default Component.extend({
           return a.get('endDate') - b.get('endDate');
         });
       });
-console.log(groups)
+
       return groups;
     }
-  })
+  }),
+
+  earliestDate: computed({
+    get() {
+      return this._determineDateBracket(Math.min);
+    }
+  }),
+
+  latestDate: computed({
+    get() {
+      return this._determineDateBracket(Math.max);
+    }
+  }),
+
+  _determineDateBracket(minOrMax) {
+    const groups = this.get('userIdentityGroups');
+
+    return Object.keys(groups).reduce((overallDate, key) => {
+      return groups.get(key).reduce((groupDate, userIdentity) => {
+        const { endDate, startDate } = userIdentity.getProperties('endDate', 'startDate');
+        const date = isNone(endDate) ? startDate : isNone(startDate) ? endDate : minOrMax(endDate, startDate);
+        return isNone(groupDate) ? date : isNone(date) ? groupDate : minOrMax(date, groupDate);
+      }, overallDate);
+    }, null);
+  }
 });
