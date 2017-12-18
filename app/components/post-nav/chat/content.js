278468bc48db8798d5fb7a0988599a03349042a0
null null null <null@affinitygamescollective.org>
2017-12-18 10:42:45 -0500

    initial comment styling

--- app/components/post-nav/chat/content.js
+++ app/components/post-nav/chat/content.js
@@ -2,6 +2,8 @@ import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['comments'],
+
   store: service(),
 
   didReceiveAttrs(...args) {
