f87c96ab991a539ab95219fa267c8efe5cc0b6b4
null null null <null@affinitygamescollective.org>
2017-12-19 11:58:15 -0500

    collapse comment text

--- app/components/post-nav/chat/comment.js
+++ app/components/post-nav/chat/comment.js
@@ -1,10 +1,17 @@
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['comment'],
-  classNameBindings: ['isCollapsed'],
+  classNameBindings: ['isCollapsedChild'],
 
   childrenAreCollapsed: true,
+
+  textIsOverflown: computed('comment.text', 'overflowIsExpanded', {
+    get() {
+      return !this.get('overflowIsExpanded') && this.get('comment.text.length') > 500;
+    }
+  }),
 
   click(...args) {
     this._super(...args);
@@ -25,6 +32,10 @@ export default Component.extend({
   actions: {
     expandChildren() {
       this.set('childrenAreCollapsed', false);
+    },
+
+    expandOverflow() {
+      this.set('overflowIsExpanded', true);
     }
   }
 });
