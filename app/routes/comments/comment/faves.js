a7aab51825143e2e4a12522655f94f9e090f3ff3
null null null <null@affinitygamescollective.org>
2018-01-07 12:04:14 -0500

    infinite load comment faves too

--- app/routes/comments/comment/faves.js
+++ app/routes/comments/comment/faves.js
@@ -1,20 +1,21 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import InfinityRoute from "ember-infinity/mixins/route";
 import RouteTitleMixin from 'client/mixins/route-title';
 
-export default Route.extend(RouteTitleMixin, {
+export default Route.extend(InfinityRoute, RouteTitleMixin, {
   linkRoute: 'comments.comment',
 
   intl: service(),
 
   model() {
-    return this.store.query('fav', { favableId: this.modelFor('comments.comment').id, favableType: 'comment', include: 'user, user.userProfile' });
+    return this.infinityModel('fav', { perPage: 12, startingPage: 1, favableId: this.modelFor('comments.comment').id, favableType: 'comment', include: 'user, user.userProfile' });
   },
 
   beforeModel(...args) {
     this._super(...args);
 
     this.set('linkModelId', this.modelFor('comments.comment').id);
-    this.set('titleToken', this.get('intl').t('comments.faves'));
+    this.set('titleToken', this.get('intl').t('comment.faves'));
   }
 });
