12697524c257e9b8452861921dc0b46472e423ab
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-01-17 10:00:56 -0500

    list flags individually

---
+++ app/components/violation-list.js
@@ -0,0 +1,27 @@
+import Component from '@ember/component';
+import EmberObject, { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  tagName: '',
+
+  intl: service(),
+
+  hasViolations: computed({
+    get() {
+      return this.get('violations.length') > 0;
+    }
+  }),
+
+  violations: computed({
+    get() {
+      const { flag, intl } = this.getProperties('flag', 'intl');
+
+      return ['bigotry', 'bot', 'harassment', 'sleaze', 'threat', 'unconsentingImage', 'unmarkedNSFW'].reduce((accumulator, violation) => {
+        if (flag.get(violation)) accumulator.push(intl.t(`flags.attributes.${violation}.name`));
+
+        return accumulator;
+      }, []);
+    }
+  })
+});
