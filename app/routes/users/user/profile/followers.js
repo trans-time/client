e7e3ce724ca3c8dbab3a340fbf10fb463af4ae21
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-02-04 18:41:45 -0500

    hook up follows to api

--- app/routes/users/user/profile/followers.js
+++ app/routes/users/user/profile/followers.js
@@ -2,9 +2,11 @@ import Route from '@ember/routing/route';
 import InfinityRoute from "ember-infinity/mixins/route";
 
 export default Route.extend(InfinityRoute, {
+  perPageParam: 'page_size',
+
   model() {
     const user = this.modelFor('users.user');
 
-    return this.infinityModel('follow', { perPage: 12, startingPage: 1, followedId: user.id, include: 'follower, follower.userProfile' });
+    return this.infinityModel('follow', { perPage: 10, startingPage: 1, filter: { followed_id: user.id }, include: 'follower' });
   }
 });
--- app/routes/users/user/profile/following.js
+++ app/routes/users/user/profile/following.js
@@ -2,9 +2,11 @@ import Route from '@ember/routing/route';
 import InfinityRoute from "ember-infinity/mixins/route";
 
 export default Route.extend(InfinityRoute, {
+  perPageParam: 'page_size',
+
   model() {
     const user = this.modelFor('users.user');
 
-    return this.infinityModel('follow', { perPage: 12, startingPage: 1, followerId: user.id, include: 'followed, followed.userProfile' });
+    return this.infinityModel('follow', { perPage: 10, startingPage: 1, filter: { follower_id: user.id }, include: 'followed,followed.user_profile' });
   }
 });
