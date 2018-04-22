0c9cde83c576221c5b562f4b75944e89d7dbf38d
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-04-22 12:01:07 -0400

    prevent key responders from activating while the modal is open

--- app/components/main-modal.js
+++ app/components/main-modal.js
@@ -1,10 +1,17 @@
-import { alias, oneWay } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
 import Component from '@ember/component';
+import { alias, oneWay, notEmpty } from '@ember/object/computed';
+import { on } from '@ember/object/evented';
+import { inject as service } from '@ember/service';
+import { EKMixin, EKOnInsertMixin, keyDown } from 'ember-keyboard';
 
-export default Component.extend({
+export default Component.extend(EKMixin, EKOnInsertMixin, {
   modalManager: service(),
 
   componentPath: oneWay('modalManager.componentPath'),
-  options: alias('modalManager.options')
+  keyboardFirstResponder: notEmpty('componentPath'),
+  options: alias('modalManager.options'),
+
+  _keyClose: on(keyDown('Escape'), function() {
+    this.get('modalManager').close('reject');
+  })
 });
