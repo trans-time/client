e0ba91afe3dd931f512ebbaa917c5699b79af2dc
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-07-02 17:45:40 -0400

    try again to backgroundReload

--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.store.findRecord('post', params.id, { include: 'timeline_item,timeline_item.user,images' });
+    return this.store.findRecord('post', params.id, { include: 'timeline_item,timeline_item.user,images', backgroundReload: true });
   }
 });
