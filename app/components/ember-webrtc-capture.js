44b5c17d52744e2d57fad3a2b8e8261292293c83
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-07-03 10:11:05 -0400

    fix webrtc video on ios

--- app/components/ember-webrtc-capture.js
+++ app/components/ember-webrtc-capture.js
@@ -17,6 +17,10 @@ export default Component.extend({
     this._video.addEventListener('touchend', bind(this, () => this.get('_takePicture').perform()));
     this._video.addEventListener('click', bind(this, () => this.get('_takePicture').perform()));
 
+    this._video.setAttribute('autoplay', '');
+    this._video.setAttribute('muted', '');
+    this._video.setAttribute('playsinline', '');
+
     navigator.mediaDevices.enumerateDevices().then((devices) => {
       this._devices = devices.filter((device) => device.kind === 'videoinput');
       this._startCamera();
