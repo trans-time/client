a593b58f12f8322ee3037608b9c5e18da4b8259e
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-07-05 13:19:38 -0400

    safeguard against relationship user-tag-summaries that do not yet exist

--- app/routes/users/user/profile/index.js
+++ app/routes/users/user/profile/index.js
@@ -12,13 +12,15 @@ export default Route.extend({
   },
 
   model(params) {
-    return this.store.query('user-tag-summary', { filter: { subject_id: this.modelFor('users.user').id, author_usernames: [this.modelFor('users.user').get('username'), ...params.relationships] }, include: 'subject,author,user_tag_summary_tags,user_tag_summary_tags.tag,user_tag_summary_users,user_tag_summary_users.user', reload: true });
+    return this.store.query('user-tag-summary', { filter: { subject_id: this.modelFor('users.user').id, author_usernames: [this.modelFor('users.user').get('username'), ...params.relationships] }, include: 'subject,author,user_tag_summary_tags,user_tag_summary_tags.tag,user_tag_summary_users,user_tag_summary_users.user' });
   },
 
   actions: {
     addUserTagSummary(author_username) {
-      this.store.query('user-tag-summary', { filter: { subject_id: this.modelFor('users.user').id, author_username }, include: 'subject,author,user_tag_summary_tags,user_tag_summary_tags.tag,user_tag_summary_users,user_tag_summary_users.user', reload: true }).then((userTagSummary) => {
-        this.controllerFor('users.user.profile.index').get('model').pushObject(userTagSummary.get('content.firstObject'));
+      this.store.query('user-tag-summary', { filter: { subject_id: this.modelFor('users.user').id, author_username }, include: 'subject,author,user_tag_summary_tags,user_tag_summary_tags.tag,user_tag_summary_users,user_tag_summary_users.user' }).then((userTagSummary) => {
+        if (userTagSummary.get('content.length') < 0) {
+          this.controllerFor('users.user.profile.index').get('model').pushObject(userTagSummary.get('content.firstObject'));
+        }
       });
     }
   }
