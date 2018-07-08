3b6d3539aabec796ceec49831db0af69f48d305b
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-07-08 12:34:37 -0400

    fix last timeline-item nav

--- app/controllers/users/user/timeline.js
+++ app/controllers/users/user/timeline.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 import TimelineItemNavControllerMixin from 'client/mixins/timeline-item-nav-controller';
 
 export default Controller.extend(TimelineItemNavControllerMixin, {
-  queryParams: ['timelineItemId', 'tags', 'relationships', 'lastPost'],
-  lastPost: null,
+  queryParams: ['timelineItemId', 'tags', 'relationships', 'lastTimelineItem'],
+  lastTimelineItem: null,
   timelineItemId: null,
   tags: [],
   relationships: []
