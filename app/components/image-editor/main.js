2475339cc7d2a27613bb149459ddef7fbaf12f0c
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-08-12 18:57:15 -0400

    rotate images

--- app/components/image-editor/main.js
+++ app/components/image-editor/main.js
@@ -5,6 +5,7 @@ import { htmlSafe } from '@ember/string';
 import { isPresent } from '@ember/utils';
 import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
+import getImageOrientation from 'client/utils/get-image-orientation';
 
 const hasWebRTCSupport = isPresent(navigator.mediaDevices);
 
@@ -53,6 +54,19 @@ export default Component.extend({
     }
   }),
 
+  _getRotation(orientation) {
+    switch(orientation) {
+      case 1: return 0;
+      case 2: return 0;
+      case 3: return 180;
+      case 4: return 180;
+      case 5: return 90;
+      case 6: return 90;
+      case 7: return 270;
+      case 8: return 270;
+    }
+  },
+
   actions: {
     deleteImage(image) {
       this.attrs.removeImage(image);
@@ -81,8 +95,30 @@ export default Component.extend({
     },
 
     uploadImage(file) {
-      file.readAsDataURL().then((dataUri) => {
-        this.attrs.addImage(dataUri);
+      getImageOrientation(file.blob, (orientation) => {
+        if (orientation === 1) {
+          file.readAsDataURL().then((dataUri) => {
+            this.attrs.addImage(dataUri);
+          });
+        } else {
+          const image = new Image();
+          const rotation = this._getRotation(orientation);
+
+          image.onload = () => {
+            const canvas = document.createElement('canvas');
+            canvas.height = [0, 180].indexOf(rotation) > -1 ? image.height : image.width;
+            canvas.width =  [0, 180].indexOf(rotation) > -1 ? image.width : image.height;
+
+            const ctx = canvas.getContext('2d');
+
+            ctx.translate(canvas.width / 2,canvas.height / 2);
+            ctx.rotate(rotation * Math.PI/180);
+            if ([2,4,5,7].indexOf(orientation) > -1) ctx.scale(-1, 1);
+            ctx.drawImage(image, -image.width / 2, -image.height / 2);
+            this.attrs.addImage(canvas.toDataURL(file.blob.type, 1.0));
+          }
+          image.src = URL.createObjectURL(file.blob);
+        }
       });
     }
   }
---
+++ tests/unit/utils/get-image-orientation-test.js
@@ -0,0 +1,10 @@
+import getImageOrientation from 'client/utils/get-image-orientation';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | get image orientation');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getImageOrientation();
+  assert.ok(result);
+});
