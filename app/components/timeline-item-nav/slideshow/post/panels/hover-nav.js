a21f5e3571075d3a7387c5db36925f0d591ba18e
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-11-05 10:43:19 -0500

    touch nav for mobile

--- app/components/timeline-item-nav/slideshow/post/panels/hover-nav.js
+++ app/components/timeline-item-nav/slideshow/post/panels/hover-nav.js
@@ -1,9 +1,26 @@
 import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
   classNames: 'hover-nav',
 
   click() {
     this.scroll(this.direction);
-  }
+  },
+
+  touchStart() {
+    this.touchTimerTask.perform();
+  },
+
+  touchEnd() {
+    if (this.touchActivated) this.scroll(this.direction);
+  },
+
+  touchTimerTask: task(function * () {
+    this.set('touchActivated', true);
+
+    yield timeout(500);
+
+    this.set('touchActivated', false);
+  }).restartable()
 });
