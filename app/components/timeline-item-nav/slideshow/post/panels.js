229bf60501614069deb13805c1dd4ccb2740656f
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-11-05 10:24:20 -0500

    account for scrollbar width when cycling

--- app/components/timeline-item-nav/slideshow/post/panels.js
+++ app/components/timeline-item-nav/slideshow/post/panels.js
@@ -59,9 +59,10 @@ export default Component.extend({
 
   actions: {
     scrollHorizontal(direction) {
-      if (direction > 0 && this._carousel.scrollLeft + this._carousel.clientWidth + 45 >= this._carousel.scrollWidth) {
+      const scrollbarWidth = 45;
+      if (direction > 0 && this._carousel.scrollLeft + this._carousel.clientWidth + scrollbarWidth >= this._carousel.scrollWidth) {
         this._carousel.scrollLeft = 0;
-      } else if (direction < 0 && this._carousel.scrollLeft === 0) {
+      } else if (direction < 0 && this._carousel.scrollLeft <= scrollbarWidth) {
         this._carousel.scrollLeft = this._carousel.scrollWidth;
       } else {
         this._carousel.scrollLeft += direction * this.element.querySelector('.timeline-item-nav-slideshow-panel').clientWidth;
