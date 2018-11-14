7dd0ab842bc23069c890bf42ca294deb2bfa2800
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-11-14 10:07:50 -0500

    safari support

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -39,9 +39,9 @@ module.exports = function(defaults) {
     hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild
   });
 
-  app.import('bower_components/lethargy/lethargy.js');
   app.import('bower_components/linkifyjs/linkify.js');
   app.import('bower_components/linkifyjs/linkify-html.js');
+  app.import('node_modules/intersection-observer/intersection-observer.js');
   app.import('node_modules/croppie/croppie.css');
   app.import('node_modules/croppie/croppie.js', {
     using: [
